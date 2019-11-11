3b9dd70007ae97f0c56bcc359511c23938138f99
ZhenzeZhao <c00198551@itcarlow.ie>
2019-11-12 00:21:56 +0800

    first feature

---
+++ BalloonObject/Assets/Scripts/BalloonMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonMovement : MonoBehaviour {
+
+    public float force;
+
+    private Rigidbody2D rb2d;
+    private float moveVertical = 0;
+
+    // Use this for initialization
+    void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+
+    public static float getVelocity(float force, float mass)
+    {
+        float acceleration = (force - mass * 10) / mass; // force = mass * acceleration
+
+        float velocity = acceleration * Time.deltaTime; // velocity = acceleration * time
+        
+        return velocity;
+    }
+
+    void FixedUpdate()
+    {
+        moveVertical += getVelocity(force, rb2d.mass);
+
+        Vector2 movement = new Vector2(0, moveVertical);
+
+        rb2d.AddForce(movement * Time.deltaTime); // 
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
