5f8df7c7dd63fb9788b0e17222a85ee707fcd321
zhenze zhao <c00198551@itcarlow.ie>
2019-11-26 16:45:29 +0000

    remove lib

---
+++ BalloonObject/Assets/Scripts/CarryCheck.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarryCheck : MonoBehaviour
+{
+    public GameObject wire;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "box")
+        {
+            Debug.Log("check");
+            wire.GetComponent<HingeJoint2D>().connectedBody = collision.GetComponent<Rigidbody2D>();
+        }    
+    }
+
+}
