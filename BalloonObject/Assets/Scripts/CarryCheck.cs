b3cfb9dcb0098bb599b28386a6812f32b27d59d6
zhenze zhao <c00198551@itcarlow.ie>
2020-01-21 11:13:54 +0000

    upload changes

--- BalloonObject/Assets/Scripts/CarryCheck.cs
+++ BalloonObject/Assets/Scripts/CarryCheck.cs
@@ -1,17 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class CarryCheck : MonoBehaviour
 {
-    public GameObject wire;
+    private bool onload = false;
+    private bool release = false;
+    private string goods = " ";
+    private GameObject player;
+
+    private void Update()
+    {
+
+        if (Input.GetKeyDown("space") && onload && goods == "Player")
+        {
+            Debug.Log(goods);
+            release = true;
+            Destroy(player.GetComponent<HingeJoint2D>());
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "box")
+        if ((collision.gameObject.tag == "Player" || collision.gameObject.tag == "Box")
+            && !onload)
         {
-            Debug.Log("check");
-            wire.GetComponent<HingeJoint2D>().connectedBody = collision.GetComponent<Rigidbody2D>();
+            goods = collision.gameObject.tag;
+            player = collision.gameObject;
+            collision.gameObject.AddComponent<HingeJoint2D>();
+            collision.gameObject.GetComponent<HingeJoint2D>().connectedBody = this.GetComponent<Rigidbody2D>();
+            onload = true;
         }    
     }
 
